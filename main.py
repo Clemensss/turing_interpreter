9116ed6cf1733745db823372a535ea4d6e797e90
Clemensss <cass@ic.ufal.br>
2020-02-02 03:15:25 -0300

    comentarios

--- main.py
+++ main.py
@@ -6,37 +6,56 @@ index = 0
 preprogramed = ["SCAN", "PRT", "DIR", "ESQ", "HALT"]
 halt = False
 
+
+'''
+    tira todos os espacos e salva o o comando na sub_list,
+    depois de ler HALT manda a lista de comandos para o dicionario
+    de subrotinas
+'''
+
 def add_new_sub(content, sub_name, i):
     sub_list = []
     while True:
         if content[i] == "HALT":
             break
-        sub_list.append(content[i])
+        sub_list.append(''.join(content[i].split()))
         i+=1
 
     subroutines[sub_name] = sub_list
 
     return i            
 
+'''
+    pega a lista de strings content e procura pelas
+    palavras chave DEFINE e MAIN, se acha uma das 
+    duas define uma subrotina com todos os comandos
+    entre ela e HALT
+'''
 def make_subroutines(content):
 
     i = 0
     while i < len(content):
-        lista = content[i].split(' ')
+        lista = content[i].split(':')
 
-        if lista[0] == "DEFINE:":
+        if lista[0] == "DEFINE":
             i = add_new_sub(content, lista[1].strip(), i+1)
-        elif lista[0] == "MAIN:":
+        elif lista[0] == "MAIN":
             i = add_new_sub(content, lista[0], i+1)
         
         i+=1
 
+'''
+    le o arquivo e separa ele por cada linha, ja tirando o
+    \n, depois faz uma limpeza em cada linha, pra tirar
+    linhas vazias e depois uma pra tirar todos os whitespaces
+'''
 def read_file(file_name):
 
     f = open(file_name, "r")
 
     content = f.read().splitlines()
     content = [x for x in content if x.strip()]
+    content = [x for x in content if x[0] != '#']
     content = [x.strip() for x in content]
 
     print(content)
@@ -50,9 +69,10 @@ def print_dict(var):
         for y in var[x]:
             print (y)
 
+'''roda as funcoes ja preprogramadas'''
 def run_preprogramed(inst, name):
 
-    inst = inst.split(' ')
+    inst = inst.split(':')
     if inst[0] == "SCAN":
         if len(inst) != 2:
             print("ERROR: Too many or too few arguments for SCAN in {}".format(name[:-1]))
@@ -75,17 +95,24 @@ def run_preprogramed(inst, name):
     elif inst[0] == "ESQ":
         ESQ()
         return False
+'''
+   roda uma subrotina qualquer, se dentro da subrotina houver
+   outra subrotina definida pelo usuario, a funcao e chamada de
+   novo recursivamente, se for uma funcao preprogramada
+   vai executar ela normalmente
 
+   i eh o index de comandos, comeca de 0
+'''
 def run_sub(name):
     inst_l = subroutines[name]
     i = 0
     global halt
 
     while True:
-        inst = inst_l[i].split(", ")
+        inst = inst_l[i].split(",")
         ver = False
 
-        if inst[0].split(' ')[0] not in preprogramed:
+        if inst[0].split(':')[0] not in preprogramed:
             if inst[0] in subroutines: 
                 run_sub(inst[0])
             else:
@@ -95,20 +122,16 @@ def run_sub(name):
         else:
             ver = run_preprogramed(inst[0], name)
 
-            if halt:
-                print("HALT")
-                break
-        
-        
-        print(''.join(fita_turing))
-        if ver and len(inst) > 1:
+        if ver:
             i = int(inst[1])
         else:
             i+=1
-
+        
         if i == len(inst_l):
             break
                 
+    print(''.join(fita_turing))
+
 def SCAN(char):
     global index
     if char == fita_turing[index]:
@@ -136,13 +159,12 @@ def ESQ():
         index -= 1
 
 def turing():
-    run_sub("MAIN:")
+    run_sub("MAIN")
 
 def main():
     read_file(sys.argv[1])
 
     turing()
-    print(''.join(fita_turing))
 
 if __name__ == "__main__":
     main()
